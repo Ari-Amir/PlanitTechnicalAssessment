413b08ef9ecb1875a81edfb22f762ae86506733f
Arystan <arystan.amirov@gmail.com>
2025-03-01 14:19:53 +1300

    Update Jenkins file

--- PlanitTechnicalAssessment/SequentialTestCollection.cs
+++
@@ -1,5 +0,0 @@
-﻿using Xunit;
-
-// Атрибут для отключения параллельности выполнения тестов
-[CollectionDefinition("Sequential Tests", DisableParallelization = true)]
-public class SequentialTestCollection { }
